5ac70041b3adb143d03167ebb6668284808a0eb8
Jmarkus <Jmarkus.fowler@gmail.com>
2024-10-22 21:26:22 -0400

    Final product. Full lead and tab save

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+        console.log(tabs)
+    })
+})
+
 
 function render(leads) {
     let listItems = ""
